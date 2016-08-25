65ec30a181b12729c6a51a591d27305d24ccbcd9
gitsomecode <sjbitcode@gmail.com>
2016-08-25 18:22:35 -0400

    Edit secret_key_gen.py to generate key and add it to .bash_profile, gitignore .vagrant

--- scripts/secret_key_gen.py
+++ scripts/secret_key_gen.py
@@ -3,4 +3,5 @@ import os
 
 DJANGO_SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
 
-os.environ['SECRET_KEY'] = DJANGO_SECRET_KEY
+with open(os.path.expanduser('~/.bash_profile'), 'a') as outfile:
+    outfile.write('export SECRET_KEY=' + repr(DJANGO_SECRET_KEY))
